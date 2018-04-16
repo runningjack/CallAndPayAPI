e239bec1954d23438cd361bb1d9bac9d2c3a6c62
runningjack <amedora09@gmail.com>
2018-04-16 20:30:35 +0100

    CallAndPayAPI added

---
+++ CallAndPayAPI/Controllers/CustomersController.cs
@@ -0,0 +1,120 @@
+﻿using BLL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CallAndPayAPI.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private CustomerRepository _customerRepository;
+
+        public CustomersController()
+        {
+            _customerRepository = new CustomerRepository();
+        }
+
+        // GET: api/Customers
+        [AcceptVerbs("GET")]
+        [Route("api/Customers")]
+        [ResponseType(typeof(Customer))]
+        public IHttpActionResult GetCustomers()
+        {
+            var cutomers = _customerRepository.ListCustomers();
+            return Ok(cutomers.ToList());
+        }
+
+        // GET: api/Customers/5
+        [AcceptVerbs("GET")]
+        [Route("api/Customers/GetCustomer/{id}")]
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> GetCustomer(int id)
+        {
+            Customer customer = _customerRepository.FindById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        // PUT: api/Customers/5
+        [AcceptVerbs("POST")]
+        [Route("api/Customers/UpdateCustomer/")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> UpdateCustomer(Customer customer)
+        {
+            //return Ok(customer);
+            if (!ModelState.IsValid)
+            {
+               // return Json(new { Msg = "Record could not be created Internal Server Error " });
+                return BadRequest(ModelState);
+            }
+            var i = _customerRepository.Update(customer);
+            if (i > 0)
+            {
+                return Json(new { Msg = "1" });
+            }
+            //return StatusCode(HttpStatusCode.NoContent);
+            return Json(new { Msg = "0" });
+        }
+
+        // POST: api/Customers
+        [AcceptVerbs("POST")]
+        [Route("api/Customers/PostCustomer/")]
+        [ResponseType(typeof(Customer))]
+        public async Task<IHttpActionResult> PostCustomer(Customer customer)
+        {
+            //return Ok(customer.ToList());
+            return Json(new { Msg = "0" });
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var i = _customerRepository.Create(customer);
+            if (i > 0)
+            {
+                //return CreatedAtRoute("DefaultApi", new { id = customer.Id }, customer);
+                return Json(new { Msg = "1" });
+            }
+            //return null;
+            return Json(new { Msg = "0" });
+        }
+
+        // DELETE: api/Customers/5
+        [AcceptVerbs("DELETE")]
+        [Route("api/Customers/DeleteCustomer/{id}")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> DeleteCustomer(int id)
+        {
+            Customer customer = _customerRepository.FindById(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            var i = _customerRepository.Delete(customer);
+            //return Ok(customer);
+            if (i > 0)
+            {
+                return Json(new { Msg = "1" });
+            }
+            return Json(new { Msg = "0" });
+        }
+        private bool CustomerExists(int id)
+        {
+            Customer cust = _customerRepository.FindById(id);
+            if (cust != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CallAndPayAPI/Controllers/StateController.cs
@@ -0,0 +1,116 @@
+﻿using BLL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CallAndPayAPI.Controllers
+{
+    public class StateController : ApiController
+    {
+        private StatesRepository _stateRepository;
+        public StateController()
+        {
+            _stateRepository = new StatesRepository();
+        }
+        [AcceptVerbs("GET")]
+        [Route("api/States")]
+        [ResponseType(typeof(State))]
+        public IHttpActionResult GetStates()
+        {
+            var states = _stateRepository.ListStates();
+            return Ok(states.ToList());
+        }
+
+        // GET: api/States/5
+        [AcceptVerbs("GET")]
+        [Route("api/State/GetState/{id}")]
+        [ResponseType(typeof(State))]
+        public async Task<IHttpActionResult> GetState(int id)
+        {
+            State State = _stateRepository.FindById(id);
+            if (State == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(State);
+        }
+
+        // PUT: api/States/5
+        [AcceptVerbs("POST")]
+        [Route("api/States/UpdateState/")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> UpdateState(State State)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var i = _stateRepository.Update(State);
+            if (i > 0)
+            {
+                return Json(new { Msg = "1" });
+            }
+            //return StatusCode(HttpStatusCode.NoContent);
+            return Json(new { Msg = "0" });
+        }
+
+        // POST: api/States
+        [AcceptVerbs("POST")]
+        [Route("api/States/PostState/")]
+        [ResponseType(typeof(State))]
+        public async Task<IHttpActionResult> PostState(State State)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var i = _stateRepository.Create(State);
+            if (i > 0)
+            {
+                //return CreatedAtRoute("DefaultApi", new { id = State.Id }, State);
+                return Json(new { Msg = "1" });
+            }
+            //return null;
+            return Json(new { Msg = "0" });
+        }
+
+        // DELETE: api/States/5
+        [AcceptVerbs("DELETE")]
+        [Route("api/States/DeleteState/{id}")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> DeleteState(int id)
+        {
+            State State = _stateRepository.FindById(id);
+            if (State == null)
+            {
+                return NotFound();
+            }
+            var i = _stateRepository.Delete(State);
+            //return Ok(State);
+            if (i > 0)
+            {
+                return Json(new { Msg = "1" });
+            }
+            return Json(new { Msg = "0" });
+        }
+
+
+
+        private bool StateExists(int id)
+        {
+            State state = _stateRepository.FindById(id);
+            if (state != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
